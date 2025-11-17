a53a4065b38f8a6c8e72133b2b06902a3d7b4b0c
ICLOUD UNLOCKER Net <Hazique159@gmail.com>
2025-11-17 18:47:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9361053537-2008f",
+  "appId": "1:672808009448:web:4ff8d450b8ff828dd3135d",
+  "apiKey": "AIzaSyCNSGS9cdJ08ybwr0fkdgxAI3XQSi1OPHE",
+  "authDomain": "studio-9361053537-2008f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "672808009448"
+};
