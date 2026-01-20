bedb9bc77353b57f2fcbd321231478284f854182
ICLOUD UNLOCKER Net <Hazique159@gmail.com>
2026-01-20 13:33:56 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async redirects() {
-    return [
-      {
-        source: '/:path((main/complaints))',
-        destination: '/complaints',
-        permanent: false,
-      },
-    ]
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
